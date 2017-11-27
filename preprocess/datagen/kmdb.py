2e7ca218b34acc090a9c1221ede0f6e086109523
Miller Developmen <sionnachdevel@gmail.com>
2017-11-26 20:31:45 -0800

    Updated kmdb package to write in batches.

--- preprocess/datagen/kmdb.py
+++ preprocess/datagen/kmdb.py
@@ -5,16 +5,51 @@ import numpy as np
 
 
 class kmdbWriter:
-    def __init__(self, path, map_size=1.074e9, commit_freq=8):
+    def __init__(self, path, batch_size=32, map_size=1.074e9):
         self.path = path
         self.env = lmdb.open(path, map_size=map_size)
         self.txn = self.env.begin(write=True)
         self.cur_key = 0
-        self.commit_freq=10
+        self.count = 0
+        self.batch_size = batch_size
+        self.xbatch = []
+        self.ybatch = []
         
     
     def __del__(self):
         self.close()
+     
+        
+    # Reset the holding of the current batch
+    def __reset_batch(self):
+        self.count = 0
+        self.xbatch = []
+        self.ybatch = []
+        
+        
+    # Adds a point to the lmdb.
+    def write_point(self, xpoint, ypoint):
+        # Check if a batch has already been started as this changes if a batch needs to be initiated.
+        if self.count <= 0:
+            # If a batch is new then it needs to be initiated
+            for x in xpoint:
+                self.xbatch.append(np.empty((self.batch_size,) + x.shape))
+            for y in ypoint:
+                self.ybatch.append(np.empty((self.batch_size,) + y.shape))
+        
+        # Add the point the current batch
+        for i in range(0, len(self.xbatch)):
+            self.xbatch[i][self.count, ...] = xpoint[i]
+        for i in range(0, len(self.ybatch)):
+            self.ybatch[i][self.count, ...] = ypoint[i]
+        self.count += 1
+        
+        # If the batch is full commit the batch to the lmdb
+        if self.count >= self.batch_size:
+            self.write(self.xbatch, self.ybatch)
+            self.commit()
+            self.__reset_batch() # Reset the batch variables for the next batch
+            
         
         
     def write(self, x, y):
@@ -24,7 +59,6 @@ class kmdbWriter:
             data_x = []
             for elem in x:
                 data_x.append(elem.tolist())
-            print('islist')
         elif isinstance(x, np.ndarray):
             data_x = [x.tolist()]
         else:
@@ -34,8 +68,7 @@ class kmdbWriter:
         if isinstance(y, list):
             data_y = []
             for elem in y:
-                data_x.append(elem.tolist())
-            print('islist')
+                data_y.append(elem.tolist())
         elif isinstance(y, np.ndarray):
             data_y = [y.tolist()]
         else:
@@ -45,8 +78,6 @@ class kmdbWriter:
         self.txn.put(str(self.cur_key).encode('ascii'), json.dumps(data).encode('ascii')) 
         self.cur_key += 1
         
-        if self.cur_key % self.commit_freq:
-            self.commit()
         
         
     def commit(self):
@@ -59,11 +90,6 @@ class kmdbWriter:
         F = open(fname, 'w')
         F.write(str(self.cur_key))
         F.close()
-        
-        try:
-            self.txn.commit()
-        except:
-            pass
         self.env.close()
     
     
@@ -91,10 +117,9 @@ class kmdbReader:
             # Check to ensure that this can safely generate keys (# of elements > 0)
             if self.key_len <= 0:
                 raise IOError
-                
+            
             for key, value in self.cursor:
                 data = json.loads(value)
-                
                 data_x = []
                 for dat_x in data['x']:
                     data_x.append(np.array(dat_x))
@@ -104,8 +129,50 @@ class kmdbReader:
                     data_y.append(np.array(dat_y))
                 
                 yield data_x, data_y
-                
-                
+               
+        
+#    # Creates an iterator that reads in batches. Currently      
+#    def generate(self, batch_size=100):
+#        while(True):
+#            gen = self.rand_generate()
+#            xlist, ylist = next(gen)
+#            
+#            # Create batch list and np array for each input
+#            xbatch = []
+#            for xpoint in xlist:
+#                x = np.empty((batch_size,) + xpoint.shape)
+#                
+#                # Fill in the first values
+#                x[0, ...] = xpoint
+#                
+#                # Add the batch for this input to the array.
+#                xbatch.append(x)
+#                
+#            # Do the same thing but for outputs
+#            ybatch = []
+#            for ypoint in ylist:
+#                y = np.empty((batch_size,) + ypoint.shape)
+#                
+#                # Fill in the first values
+#                y[0, ...] = ypoint
+#                
+#                ybatch.append(y)
+#                
+#            # Fill in the rest of the batch values
+#            for i in range(1, batch_size):
+#                xlist, ylist = next(gen)
+#                
+#                # Fill in x values
+#                for j in range(0, len(xbatch)):
+#                    xbatch[j][i, ...] = xlist[j]
+#    
+#                # Fill in y valuess
+#                for j in range(0, len(ybatch)):
+#                    ybatch[j][i, ...] = ylist[j]
+#    
+#            yield xbatch, ybatch
+
+
     # Creates an iterator that reads through all the elements in a random order before starting again
     def rand_generate(self):
         while True:
